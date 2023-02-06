9f9c8841ff909247bea59ea4f9626e51dbf9b46e
zokka12 <120386470+zokka12@users.noreply.github.com>
2023-02-06 13:37:11 +0100

    Add files via upload

---
+++ kartka.js
@@ -0,0 +1,29 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+
+
+loadSprite("scena","kartka.png")
+
+
+
+
+loadSprite("balwan","balwan.png")
+
+
+
+
+add([
+    sprite("scena"),
+    pos(0,0)
+])
+
+
+
+
+add(
+    sprire("balwan"),
+    pos(0,0)
+)
